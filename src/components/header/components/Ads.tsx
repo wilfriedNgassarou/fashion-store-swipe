bc60840de499b28c0cbea55fb77b52b310b5ad98
WilfriedNgassarou <wilfriedngassarou23@gmail.com>
2024-05-20 13:08:06 +0200

    Ajout de credits

--- src/components/header/components/Ads.tsx
+++ src/components/header/components/Ads.tsx
@@ -2,6 +2,14 @@ export default function Ads() {
   return (
     <div className="ads">
       New here? Save 20% with code: YR24
+      <div className="credits">
+        <span>
+          Coded by <a href="https://x.com/Wilfried_Ng23" target="_blank">Wilfried Ngassarou</a>
+        </span>
+        <span>
+          Designed by <a href="https://x.com/EmaniUi" target="_blank">Emani UI</a>
+        </span>
+      </div>
     </div>
   )
 }
